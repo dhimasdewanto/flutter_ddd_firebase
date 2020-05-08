d74257426f75fb2a291c8dd08156c7930f75eedb
Dhimas Dewanto <dhimasbrd@gmail.com>
2020-05-08 14:22:03 +0700

    Fix linter and depreceated material design

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -26,3 +26,24 @@ abstract class ValueObject<T> extends Equatable {
     );
   }
 }
+
+/// Try using freezed
+// part 'value_objects.freezed.dart';
+
+// @freezed
+// abstract class ValueObject<T> with _$ValueObject<T> {
+//   const factory ValueObject(int test) = _ValueObject<T>;
+
+//   Either<ValueFailure<T>, T> get value;
+
+//   bool get isValid => value.isRight();
+
+//   /// Get value or throw [UnexpectedValueError] containing [ValueFailure]
+//   T get valueOrCrash {
+//     return value.fold(
+//       (fail) => throw UnexpectedValueError(fail),
+//       // id = identity - same as writing (right) => right
+//       id,
+//     );
+//   }
+// }
